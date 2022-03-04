665fc8bf220afb1d714b7f08fd69e0731487083e
yasserox171 <yasser7aziz@gmail.com>
2022-03-04 13:08:24 +0100

    WordpressTheme

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var myCarousel = document.querySelector('#carouselExampleIndicators')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 1000,
+  wrap: false
+})
---
+++ woocommerce.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<div class="container pt-5">
+    <?php woocommerce_content(); ?>
+</div>
+<?php get_footer(); ?>
